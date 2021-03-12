5f3acc86f6bef9215e641902ce0f7f6df71f3d70
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-12 20:56:07 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -3,7 +3,7 @@ import os
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config
+    from sample_config import Config
 
 from pyrogram import Client, filters    
 
